16d1ec51d2043bf9041c6b1fd5590670e508df88
MaTTalv001 <pomeranian1981@gmail.com>
2024-02-03 23:15:01 +0900

    バグフィックス

--- frontend/app/src/components/Works.jsx
+++ frontend/app/src/components/Works.jsx
@@ -2,32 +2,44 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "preline/preline";
 import PaginationButton from './PaginationButton';
-
+//Twitterアイコン（結果的に使っていない）
 import {
   Ripple,
   initTE,
 } from "tw-elements";
-
 initTE({ Ripple });
 
 const Works = () => {
   const [works, setWorks] = useState([]); // アニメ作品のデータを保持するための状態
   const [isLoading, setIsLoading] = useState(true); // データ取得中かどうかの状態
-  const handleError = (e) => {
-    e.target.src = "/default-image.png";
+  const currentYear = new Date().getFullYear(); //現在の西暦を取得
+  const [page, setPage] = useState(1); //ページ番号を設定
+  const [searchExecuted, setSearchExecuted] = useState(false); //検索ボタンが押されたかの状態
+  const [searchKeyword, setSearchKeyword] = useState(''); //検索ワードの状態
+  const [selectedYear, setSelectedYear] = useState(''); //検索年の状態
+  const [selectedSeason, setSelectedSeason] = useState(''); //検索シーズン
+  //現在時期を抽出する
+  const getCurrentYearAndSeason = () => {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    let currentSeason;
+    if (currentMonth >= 0 && currentMonth <= 2) {
+      currentSeason = 'winter';
+    } else if (currentMonth >= 3 && currentMonth <= 5) {
+      currentSeason = 'spring';
+    } else if (currentMonth >= 6 && currentMonth <= 8) {
+      currentSeason = 'summer';
+    } else {
+      currentSeason = 'autumn';
+    }
+    return { currentYear, currentSeason };
   };
-
-  const [page, setPage] = useState(1);
-  const [searchKeyword, setSearchKeyword] = useState('');
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
-  const [selectedSeason, setSelectedSeason] = useState('');
-  
   // 年の選択肢を生成 (2000年から現在年まで)
-  const years = Array.from(new Array(new Date().getFullYear() - 1999), (val, index) => 2000 + index);
-  
+  const years = ['年を選択', ...Array.from(new Array(currentYear - 1999), (val, index) => (2000 + index+1).toString())];
   // 季節の選択肢
-  const seasons = ['winter', 'spring', 'summer', 'autumn'];
-  
+  const seasons = ['季節を選択', 'winter', 'spring', 'summer', 'autumn'];
+  // 検索を実施
   const fetchWorks = async (page, searchKeyword, search_season = null) => {
     let url = `http://localhost:3001/works?page=${page}`;
     if (searchKeyword) {
@@ -37,181 +49,191 @@ const Works = () => {
     if (search_season) {
       url += `&filter_season=${encodeURIComponent(search_season)}`;
     }
-    console.log('Request URL:', url); 
-  try {
-    const response = await axios.get(url);
-    setWorks(response.data);
-    setIsLoading(false);
-  } catch (error) {
-    console.error('Error fetching data: ', error);
-    setIsLoading(false);
-  }
-};
-    const handleNextPage = () => {
-      const nextPage = page + 1;
-      setPage(nextPage);
+    console.log('Request URL:', url);
+    try {
+      const response = await axios.get(url);
+      setWorks(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+      setIsLoading(false);
+    }
+  };
+  // nextボタンを押した時の設定
+  const handleNextPage = () => {
+    const nextPage = page + 1;
+    setPage(nextPage);
+    if (searchExecuted) {
+      fetchWorks(nextPage, searchKeyword, `${selectedYear}-${selectedSeason}`);
+    } else {
       fetchWorks(nextPage, searchKeyword);
-    };
-
-    const handlePrevPage = () => {
-      const prevPage = page - 1;
-      setPage(prevPage);
+    }
+  };
+  // prevボタンを押した時の設定
+  const handlePrevPage = () => {
+    const prevPage = page - 1;
+    setPage(prevPage);
+    if (searchExecuted) {
+      fetchWorks(prevPage, searchKeyword, `${selectedYear}-${selectedSeason}`);
+    } else {
       fetchWorks(prevPage, searchKeyword);
-    };
+    }
+  };
+
+  //リセットアクション
+  const handleResetAndReload = () => {
+    // 検索項目の状態をリセット
+    setSelectedYear('年を選択');
+    setSelectedSeason('季節を選択');
+    setSearchKeyword('');
+    // ページをリロード
+    window.location.reload();
+  };
   
   // 検索ハンドラ
   const handleSearch = () => {
-  console.log('Searching for:', searchKeyword);
-  setPage(1); // 新しい検索のためにページをリセット
-
+    if (selectedYear === '年を選択' || selectedSeason === '季節を選択') {
+      alert('仕様上、年と季節の両方選択する必要があります');
+      return;
+    }
+    setPage(1); // 新しい検索のためにページをリセット
     // 年と季節が選択されている場合、filter_seasonを追加して検索
     // 年と季節が選択されている場合、search_seasonを定義
-  const search_season = selectedYear && selectedSeason ? `${selectedYear}-${selectedSeason}` : null;
+    const search_season = selectedYear && selectedSeason ? `${selectedYear}-${selectedSeason}` : null;
     if (search_season) {
-    fetchWorks(1, searchKeyword, search_season);
-  } else {
-    // 年と季節が選択されていない場合、通常の検索を実行
-    fetchWorks(1, searchKeyword);
-  }
-};
+      fetchWorks(1, searchKeyword, search_season);
+    } else {
+      // 年と季節が選択されていない場合、通常の検索を実行
+      fetchWorks(1, searchKeyword);
+    }
+    setSearchExecuted(true); // 検索が実行されたことを示す
+  };
+  useEffect(() => {
+    const { currentYear, currentSeason } = getCurrentYearAndSeason();
+    // 現在の年と季節で初回の検索を実行
+    fetchWorks(1, '', `${currentYear}-${currentSeason}`);
+  }, []); // 依存配列を空にすることで、コンポーネントのマウント時にのみ実行
 
-    useEffect(() => {
-      fetchWorks(page);
-    }, [page]);
-
+  //loading画面
   if (isLoading) {
     return (
       <div className="bg-gray-400 min-h-screen w-full flex items-center justify-center">
-  <div type="button" className="py-3 px-4 inline-flex items-center gap-x-2 text-lg font-semibold rounded-lg border border-transparent bg-gray-500 text-white disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-    <span className="animate-spin inline-block w-10 h-10 border-[3px] border-current border-t-transparent text-white rounded-full" role="status" aria-label="loading"></span>
-    Loading
-  </div>
-</div>
+        <div type="button" className="py-3 px-4 inline-flex items-center gap-x-2 text-lg font-semibold rounded-lg border border-transparent bg-gray-500 text-white disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+          <span className="animate-spin inline-block w-10 h-10 border-[3px] border-current border-t-transparent text-white rounded-full" role="status" aria-label="loading"></span>
+          Loading
+        </div>
+      </div>
     );
   }
 
+  //メインのレンダリング画面
   return (
     <div className="bg-gray-400 min-h-screen w-full">
-      
-      
-
       <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-
         <div className="search-form my-4 mx-2">
-        <label htmlFor="hs-trailing-button-add-on-with-icon-and-button" className="sr-only">Label</label>
-        <div className="relative flex rounded-lg shadow-sm">
-          <input
-            type="text"
-            id="hs-trailing-button-add-on-with-icon-and-button"
-            name="hs-trailing-button-add-on-with-icon-and-button"
-            className="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-l-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-            placeholder="タイトル検索"
-            value={searchKeyword}
+          <label htmlFor="hs-trailing-button-add-on-with-icon-and-button" className="sr-only">Label</label>
+          <div className="relative flex rounded-lg shadow-sm">
+            <input
+              type="text"
+              id="hs-trailing-button-add-on-with-icon-and-button"
+              name="hs-trailing-button-add-on-with-icon-and-button"
+              className="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-l-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+              placeholder="タイトル検索"
+              value={searchKeyword}
               onChange={(e) => setSearchKeyword(e.target.value)}
               onKeyDown={(e) => {
-      if (e.key === 'Enter') {
-        e.preventDefault(); // エンターキーのデフォルトの動作を防止
-        handleSearch();
-      }
-    }}
+                if (e.key === 'Enter') {
+                  e.preventDefault(); // エンターキーのデフォルトの動作を防止
+                  handleSearch();
+                }
+              }}
             />
+            {/* 年の選択 */}
+            <select
+              value={selectedYear}
+              onChange={(e) => setSelectedYear(e.target.value)}
+            >
+              {years.map(year => (
+                <option key={year} value={year}>{year}</option>
+              ))
+              }
+            </select>
+            {/* 季節の選択 */}
+            <select
+              value={selectedSeason}
+              onChange={(e) => setSelectedSeason(e.target.value)}
+            >
+              {seasons.map(season => (
+                <option key={season} value={season}>{season}</option>
+              ))
+              }
+            </select>
             
-             {/* 年の選択 */}
-      <select
-        value={selectedYear}
-        onChange={(e) => setSelectedYear(e.target.value)}
-      >
-        {years.map(year => (
-          <option key={year} value={year}>{year}</option>
-        ))}
-      </select>
-
-      {/* 季節の選択 */}
-      <select
-        value={selectedSeason}
-        onChange={(e) => setSelectedSeason(e.target.value)}
-      >
-        {seasons.map(season => (
-          <option key={season} value={season}>{season}</option>
-        ))}
-      </select>
-        
-          <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
-            <svg className="flex-shrink-0 h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
+            <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
+              <svg className="flex-shrink-0 h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" /></svg>
+            </div>
+            <button
+              type="button"
+              className="min-w-[8rem] py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold  border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              onClick={handleSearch}
+            >
+              <p>検索</p>
+            </button>
+            <button
+              type="button"
+              className="min-w-[8rem] py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-r-md border border-transparent bg-red-600 text-white hover:bg-red-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              onClick={handleResetAndReload}
+            >
+              <p>リセット</p>
+            </button>
           </div>
-          <button
-            type="button"
-            className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-r-md border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-            onClick={handleSearch}
-          >
-            Search
-          </button>
         </div>
-      </div>
-
-
-
-
-
 
         <nav className="flex items-center gap-x-1">
-        <PaginationButton onClick={handlePrevPage} disabled={page === 1} direction="prev">
-          Previous
-        </PaginationButton>
-      <PaginationButton onClick={handleNextPage} direction="next">
-          Next
-      </PaginationButton>
-      </nav>
+          <PaginationButton onClick={handlePrevPage} disabled={page === 1} direction="prev">
+            Previous
+          </PaginationButton>
+          <PaginationButton onClick={handleNextPage} direction="next">
+            Next
+          </PaginationButton>
+        </nav>
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {works.map(work => (
             <>
               <div className="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
                 <div className="h-52 flex flex-col justify-center items-center bg-blue-600 rounded-lg">
-                  <img src={work.image_url || '/default-image.png'} alt="Description" className="object-cover h-full w-full rounded-lg" onError={(e) => e.target.src = '/default-image.png'}/>
+                  <img src={work.image_url || '/default-image.png'} alt="Description" className="object-cover h-full w-full rounded-lg" onError={(e) => e.target.src = '/default-image.png'} />
                 </div>
                 <div className="p-4 md:p-6">
                   <span className="mt-3 text-gray-500">
                     {work.year}:{work.season}
                   </span>
-                <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
-                  {work.title}
+                  <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
+                    {work.title}
                   </h3>
                 </div>
-              <div className="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
-              <a className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-es-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href={work.official_site_url} target="_blank" rel="noopener noreferrer">
-                Official Site
-              </a>
-              <a className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-ee-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href={work.twitter_url} target="_blank" rel="noopener noreferrer">
-                Official X (Twitter)
-              </a>
-  
-              </div>
-                
+                <div className="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
+                  <a className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-es-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href={work.official_site_url} target="_blank" rel="noopener noreferrer">
+                    Official Site
+                  </a>
+                  <a className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-ee-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href={work.twitter_url} target="_blank" rel="noopener noreferrer">
+                    Official X (Twitter)
+                  </a>
+                </div>
               </div>
             </>
-        ))}
+          ))
+          }
         </div>
         <nav className="flex items-center gap-x-1 ">
-  <PaginationButton onClick={handlePrevPage} disabled={page === 1} direction="prev">
-    <p>Previous</p>
-  </PaginationButton>
-  <PaginationButton onClick={handleNextPage} direction="next">
-    <p>Next</p>
-  </PaginationButton>
-</nav>
+          <PaginationButton onClick={handlePrevPage} disabled={page === 1} direction="prev">
+            <p>Previous</p>
+          </PaginationButton>
+          <PaginationButton onClick={handleNextPage} direction="next">
+            <p>Next</p>
+          </PaginationButton>
+        </nav>
       </div>
-      
-      
-
-      <div>      
-
-
-</div>
-
-
-
-
-
     </div>
   );
 };
