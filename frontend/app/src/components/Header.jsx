f8a6f83d239dcb406e8b77f4d36dd890e11a73e3
MaTTalv001 <pomeranian1981@gmail.com>
2024-02-05 22:46:16 +0900

    bug fix

--- frontend/app/src/components/Header.jsx
+++ frontend/app/src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Header = ({ isLoggedIn, handleLogout }) => {
   return (
-    <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-gray-800 text-sm py-4 ">
-      <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
-      <a class="pl-4 flex-none text-xl font-semibold text-white" href="#">ANIME DB</a>
-        <div class="flex text-white flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
+    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-gray-800 text-sm py-4 ">
+      <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+      <a className="pl-4 flex-none text-xl font-semibold text-white" href="#">ANIME DB</a>
+        <div className="flex text-white flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
         {isLoggedIn ? (
         <div>
           ログイン中
